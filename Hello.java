c415cc2c3bbc50e4a873defc0632aa615d583353
LGQ <1065250284@qq.com>
2021-04-20 16:27:36 +0800

    添加文件

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/*
+* @Author: LGQ
+* @Date:   2021-04-20 15:36:02
+* @Last Modified by:   LGQ
+* @Last Modified time: 2021-04-20 15:37:39
+*/
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello world!");
+	}
+}
